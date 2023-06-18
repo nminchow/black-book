a040445dbe6a9e2a88adbe8611ed069bf48794be
nminchow <noelminchow@gmail.com>
2023-06-18 15:41:46 -0500

    update advanced tier list and about view

--- src/views/about.ts
+++ src/views/about.ts
@@ -2,28 +2,37 @@ import {APIEmbed, CacheType, ChatInputCommandInteraction} from 'discord.js';
 import { author } from './shared';
 import { dbWrapper } from '../bot';
 
+const formatNumber = (count: number | null) => {
+  return new Intl.NumberFormat().format(count || 0);
+};
+
 const dbFields = async (db: dbWrapper) => {
   if (!db) {
     return [];
   }
   const { count: subs } = await db.from('subscriptions').select('*', { count: 'exact', head: true });
   const { count: events } = await db.from('events').select('*', { count: 'exact', head: true });
+  const { count: notificaitons } = await db.from('notifications').select('*', { count: 'exact', head: true });
   return [
     {
       name: 'Channels Listening for Events',
-      value: (subs || 0).toString(),
+      value: formatNumber(subs),
     },
     {
       name: 'Events Processed',
-      value: (events || 0).toString(),
+      value: formatNumber(events),
+    },
+    {
+      name: 'Notifications Sent',
+      value: formatNumber(notificaitons),
     },
   ];
 }
 
 const about = async (interaction: ChatInputCommandInteraction<CacheType>, db: dbWrapper) => {
   const description =
-    'The bot uses the data extracted from the game by Lothrik for his [build calculator](https://github.com/Lothrik/diablo4-build-calc). ' +
-    'His hard work is immensly appreciated! If you see problems with the data, see if they are also reflected there. ' +
+    'The bot is an open-source project available [here](https://github.com/nminchow/black-book). You can find more info on our readme. ' +
+    'I\'m always looking for code contributions and support! ' +
     'If there is something wrong with the bot itself, please let me know!\n\n You can find me in ' +
     `[the official support server](http://discord.gg/2nkfTRyvJh).\n`;
   const text =
--- src/views/nightmareTier.ts
+++ src/views/nightmareTier.ts
@@ -4,7 +4,9 @@ import { author } from './shared';
 const url = 'https://i.imgur.com/btT0n4D.jpg';
 
 const nightmareTier = () => {
+  const description = '[advanced tier-list spreadsheet](https://docs.google.com/spreadsheets/d/143tXzN_7-yoQCy7QEjo924UT2kWqH1VhWyXEsOvlwgA/htmlview#gid=0)';
   const embed: APIEmbed = {
+    description,
     url,
     author,
     image: {
