56666448817c323665e3467a878b686e0160b46d
nminchow <noelminchow@gmail.com>
2023-06-30 16:56:52 -0500

    updated tier list

--- src/views/nightmareTier.ts
+++ src/views/nightmareTier.ts
@@ -1,10 +1,11 @@
 import {APIEmbed} from 'discord.js';
 import { author } from './shared';
 
-const url = 'https://i.imgur.com/btT0n4D.jpg';
+const url = 'https://i.imgur.com/yFzORhw.png';
 
 const nightmareTier = () => {
-  const description = '[advanced tier-list spreadsheet](https://docs.google.com/spreadsheets/d/143tXzN_7-yoQCy7QEjo924UT2kWqH1VhWyXEsOvlwgA/htmlview#gid=0)';
+  const description = `[wudijo's spreadsheet](https://docs.google.com/spreadsheets/d/143tXzN_7-yoQCy7QEjo924UT2kWqH1VhWyXEsOvlwgA/htmlview#gid=0)
+  [Raxxanterax's spreadsheet (image below) ](https://docs.google.com/spreadsheets/d/1lJIPOom_2aRWkwzZHKGkkWU0mVCGV5kFZZXtgYuuYyo/htmlview?usp=sharing)`;
   const embed: APIEmbed = {
     description,
     url,
