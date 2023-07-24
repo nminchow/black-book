81e037a9c0dab47bfffa85af7ee1c3f648b35b82
Kozejin <drunkenkoz@gmail.com>
2023-07-24 01:18:11 -0400

    Update nightmareTier.ts (#15)

--- src/views/nightmareTier.ts
+++ src/views/nightmareTier.ts
@@ -1,7 +1,7 @@
 import {APIEmbed} from 'discord.js';
 import { author } from './shared';
 
-const url = 'https://i.imgur.com/yFzORhw.png';
+const url = 'https://i.imgur.com/PpEDsxA.png';
 
 const nightmareTier = () => {
   const description = `[wudijo's spreadsheet](https://docs.google.com/spreadsheets/d/143tXzN_7-yoQCy7QEjo924UT2kWqH1VhWyXEsOvlwgA/htmlview#gid=0)
