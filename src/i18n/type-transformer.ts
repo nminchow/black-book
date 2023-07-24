ef61f4b4ca092e1a0449390fce59b3958666736c
DjRama <40582326+DjRama@users.noreply.github.com>
2023-07-24 19:28:39 +0300

    Update type-transformer.ts

--- src/i18n/type-transformer.ts
+++ src/i18n/type-transformer.ts
@@ -16,7 +16,7 @@ export type LocaleMappingEntry = {
   locale: Locales,
 }
 
-export type SupportedLocale = Extract<Locale, Locale.EnglishUS | Locale.EnglishGB | Locale.SpanishES | Locale.PortugueseBR>;
+export type SupportedLocale = Extract<Locale, Locale.EnglishUS | Locale.EnglishGB | Locale.SpanishES | Locale.PortugueseBR | Locale.Russian>;
 
 type LocaleMapping = Record<SupportedLocale, LocaleMappingEntry>;
 
