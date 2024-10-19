15863eed78ed1675c0927ee99653fc041554f0b8
nminchow <noelminchow@gmail.com>
2024-10-19 18:45:28 -0500

    update season info

--- src/views/season.ts
+++ src/views/season.ts
@@ -4,15 +4,15 @@ const url = 'https://diablo4.blizzard.com/en-us/upcoming';
 
 const season = () => {
 
-  const description = `Starts: <t:1715698800:R>\nEnds (approx): <t:1722956400:D>\n
-  Loot Reborn! D4 ~~bad~~ good!\n
+  const description = `Starts: <t:1728334800:R>\nEnds (approx): <t:1736287200:D>\n
+  Vessel of Hatred is live!\n
   [More Info](${url})`;
   const embed: APIEmbed = {
-    title: 'Season of Loot Reborn',
+    title: 'Season of Hatred Rising',
     description,
     url,
     thumbnail: {
-      url: 'https://blz-contentstack-images.akamaized.net/v3/assets/bltf408a0557f4e4998/blta6d501757dc037ff/663d2ee5f64609224f6a9662/en_Partner_Logo.png?imwidth=320&imdensity=2.625'
+      url: 'https://blz-contentstack-images.akamaized.net/v3/assets/blt77f4425de611b362/blt71bd7bff23658869/66f5bc98a6f25197052d9b09/d4_s6_logo_EN.webp?'
     },
   };
 
